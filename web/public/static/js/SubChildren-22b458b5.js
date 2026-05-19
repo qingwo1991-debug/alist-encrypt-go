1d1c6200ad298366048034512999e47b4dd07100
root <root@localhost.localdomain>
2026-05-19 20:49:52 +0800

    fix: admin config panel actually included in build

---
+++ web/public/static/js/SubChildren-22b458b5.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./SubChildren.vue_vue_type_script_setup_true_lang-b7f8020c.js";import"./index-c9b8ea43.js";export{m as default};
---
+++ web/public/static/js/index-045b6268.js
@@ -0,0 +1,1 @@
+import{_ as e,i as c,j as n}from"./index-c9b8ea43.js";const t={};function r(o,a){return c(),n("div",null,"table")}const _=e(t,[["render",r]]);export{_ as default};
