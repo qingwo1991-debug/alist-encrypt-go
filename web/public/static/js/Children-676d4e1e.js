c7e7189d827f2ca4e78d82205fd1a2359f584962
root <root@localhost.localdomain>
2026-05-21 06:53:50 +0800

    fix(webdav): add sync dashboard and propfind fallback

--- web/public/static/js/Children-676d4e1e.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./Children.vue_vue_type_script_setup_true_lang-83c26f43.js";import"./SubChildren.vue_vue_type_script_setup_true_lang-b840624a.js";import"./index-8ff6df83.js";export{o as default};
--- web/public/static/js/SubChildren-22b458b5.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./SubChildren.vue_vue_type_script_setup_true_lang-b7f8020c.js";import"./index-c9b8ea43.js";export{m as default};
